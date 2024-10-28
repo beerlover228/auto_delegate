e9c8294a13b673c8401c1aedd13f56f2ec1a1f9e
Nikita <41645433+nikitatemkin2001@users.noreply.github.com>
2024-10-28 15:45:54 +0300

    Create restake.sh

---
+++ restake.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while true; do
+    echo arkeod tx distribution withdraw-all-rewards \
+  --commission \
+  --chain-id arkeo-testnet-3 \
+  --fees 5000uarkeo \
+  --from $WALLET
+    sleep 60
+    echo arkeod tx staking delegate $ARKEO_VALOPER_ADDRESS $(( $(arkeod q bank balances $ARKEO_WALLET_ADDRESS -o json | jq -r '.balances[0].amount') - 100000000 ))uarkeo \
+    --chain-id arkeo-testnet-3 \
+    --from $WALLET \
+    --fees=5000uarkeo
+    sleep 60
+done
