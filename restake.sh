90bb1ac3bb1f63b4c7dd0b634411aa00f3c0a3a1
Nikita <41645433+nikitatemkin2001@users.noreply.github.com>
2024-10-28 16:40:40 +0300

    Update restake.sh

--- restake.sh
+++ restake.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 while true; do
-    echo "y" | arkeod tx distribution withdraw-rewards "$(arkeod keys show wallet --bech val -a)" \
-    --commission \
+    echo "y" | arkeod tx distribution withdraw-rewards $(arkeod keys show wallet --keyring-backend test --bech val -a) \    --commission \
     --chain-id arkeo-testnet-3 \
     --fees 5000uarkeo \
     --from "$WALLET" \
     --keyring-backend test
-    echo !!!!!!CLAIM ALL REVARDS!!!!!!
 
+    echo !!!!!!CLAIM ALL REWARDS!!!!!!
     sleep 300
 
-    echo "y" | arkeod tx staking delegate "$ARKEO_VALOPER_ADDRESS" $(( $(arkeod q bank balances "$ARKEO_WALLET_ADDRESS">
+    echo "y" | arkeod tx staking delegate "$ARKEO_VALOPER_ADDRESS" $(( $(arkeod q bank balances "$ARKEO_WALLET_ADDRESS" -o json | jq -r '.balances[0].amount') - 100000000 ))uarkeo \
     --chain-id arkeo-testnet-3 \
     --from "$WALLET" \
     --fees=5000uarkeo \
     --keyring-backend test
-    echo !!!!!!STAKE!!!!!!
 
+    echo !!!!!!STAKE!!!!!!
     sleep 300
 done
